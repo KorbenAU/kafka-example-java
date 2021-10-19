e2e2792ba86940c3b686fe039c476e65babd8db4
Korben <gaoyuan.hello@gmail.com>
2021-10-19 21:00:01 +1100

    first commit

---
+++ src/main/java/com/github/korben/kafka/ProducerDemo.java
@@ -0,0 +1,14 @@
+package com.github.korben.kafka;
+
+/**
+ * @ClassName ProducerDemo
+ * @Description TODO
+ * @Author gy
+ * @Date 19/10/21 8:54 pm
+ **/
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
